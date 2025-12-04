047e4b78bd083644a8c38a3870f7e0e6bf07663a
Meir <office@web-style.co.il>
2025-12-04 11:47:32 +0000

    תיאור ברור של השינוי שבוצע

--- sola-donation-plugin.php
+++ sola-donation-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Sola Donation Plugin
  * Plugin URI: https://solapayments.com
  * Description: Professional bilingual (Hebrew/English) donation plugin with Sola Payments integration
- * Version: 1.2.0
+ * Version: 1.2.1
  * Author: Meir Tedgi
  * Author URI: https://yourwebsite.com
  * Text Domain: sola-donation
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('SOLA_DONATION_VERSION', '1.2.0');
+define('SOLA_DONATION_VERSION', '1.2.1');
 define('SOLA_DONATION_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('SOLA_DONATION_PLUGIN_URL', plugin_dir_url(__FILE__));
 
